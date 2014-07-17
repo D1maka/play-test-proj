d33d822ac7a97dfddc75d919d1f2de09cbfcee7b
Dmytro Veres <veres.dima@gmail.com>
2014-07-17 13:21:09 +0300

    added async action handlers

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,8 +4,12 @@ import models.Comment;
 import models.Topic;
 import play.data.Form;
 import play.data.validation.Constraints;
+import play.libs.F;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.Results.AsyncResult;
+
+import scala.concurrent.Promise;
 import views.html.*;
 
 import java.util.Date;
@@ -13,9 +17,23 @@ import java.util.List;
 
 public class Application extends Controller {
 
-    public static Result getTopics() {
-        List<Topic> topicsList = Topic.find.all();
-        return ok(topics.render(topicsList));
+    public static F.Promise<Result> getTopics() {
+        F.Promise<List<Topic>> promisedTopics = F.Promise.promise(
+                new F.Function0<List<Topic>>() {
+                    @Override
+                    public List<Topic> apply() throws Throwable {
+                        List<Topic> topics =  Topic.find.all();
+                        return topics;
+                    }
+                }
+        );
+
+        return promisedTopics.map(new F.Function<List<Topic>, Result>() {
+            @Override
+            public Result apply(List<Topic> topics) throws Throwable {
+                return ok(views.html.topics.render(topics));
+            }
+        });
     }
 
     public static Result newTopic() {
@@ -23,37 +41,57 @@ public class Application extends Controller {
         return ok(createtopic.render(form));
     }
 
-    public static Result createNewTopic() {
-        Form<NewTopicData> form = Form.form(NewTopicData.class).bindFromRequest();
+    public static F.Promise<Result> createNewTopic() {
+        final Form<NewTopicData> form = Form.form(NewTopicData.class).bindFromRequest();
 
         if (form.hasErrors()) {
-            return badRequest(createtopic.render(form));
+            return F.Promise.promise(new F.Function0<Result>() {
+                @Override
+                public Result apply() throws Throwable {
+                    return badRequest(createtopic.render(form));
+                }
+            });
         } else {
-            NewTopicData newTopicData = form.get();
-            Topic newTopic = new Topic();
-            newTopic.topicName = newTopicData.topicName;
-            newTopic.topicSubject = newTopicData.topicSubject;
-            newTopic.topicComment = newTopicData.topicComment;
-            newTopic.creationDate = new Date();
+            return F.Promise.promise(new F.Function0<Result>() {
+                @Override
+                public Result apply() throws Throwable {
+                    NewTopicData newTopicData = form.get();
+                    Topic newTopic = new Topic();
+                    newTopic.topicName = newTopicData.topicName;
+                    newTopic.topicSubject = newTopicData.topicSubject;
+                    newTopic.topicComment = newTopicData.topicComment;
+                    newTopic.creationDate = new Date();
 
-            Topic.store(newTopic);
+                    Topic.store(newTopic);
 
-            return redirect("/topics");
+                    return redirect("/topics");
+                }
+            });
         }
     }
 
-    public static Result topicPage(Long id) {
-        Topic topicData = Topic.find.byId(id);
-        Form<NewCommentData> newCommentDataForm = Form.form(NewCommentData.class);
-//        flash().put("topicId", id.toString());
-        List<Comment> comments = Topic.getTopicComments(topicData);
-        return ok(topicPage.render(topicData, comments, newCommentDataForm));
+    public static F.Promise<Result> topicPage(final Long id) {
+        F.Promise<Result> resultPromise = F.Promise.promise(new F.Function0<Result>() {
+            @Override
+            public Result apply() throws Throwable {
+                Topic topicData = Topic.find.byId(id);
+                Form<NewCommentData> newCommentDataForm = Form.form(NewCommentData.class);
+                List<Comment> comments = Topic.getTopicComments(topicData);
+                return ok(topicPage.render(topicData, comments, newCommentDataForm));
+            }
+        });
+        return resultPromise;
     }
 
-    public static Result newComment(Long topicId) {
+    public static F.Promise<Result> newComment(Long topicId) {
         Form<NewCommentData> commentForm = Form.form(NewCommentData.class).bindFromRequest();
         if (commentForm.hasErrors()) {
-            return badRequest();
+            return F.Promise.promise(new F.Function0<Result>() {
+                @Override
+                public Result apply() throws Throwable {
+                    return badRequest();
+                }
+            });
         } else {
             NewCommentData commentData = commentForm.get();
             Comment newComment = new Comment();
